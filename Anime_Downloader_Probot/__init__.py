4d0010d2214ddf15a1ee40c0f8b3a146b31af08b
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-12-02 18:29:00 +0530

    Update __init__.py

--- Anime_Downloader_Probot/__init__.py
+++ Anime_Downloader_Probot/__init__.py
@@ -6,8 +6,8 @@ from telethon import TelegramClient
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-API_ID = os.environ.get("API_ID", None)
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("TOKEN", None)
+API_ID = os.environ.get("API_ID", "21740783")
+API_HASH = os.environ.get("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
+BOT_TOKEN = os.environ.get("TOKEN", "7766709030:AAEHnIF6EkNttAij4cOCZat74PMK5Ymm6is")
 
 GogoAnime = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
